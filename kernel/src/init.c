49bcc511ac399ddb7cb340a2cd20c412a6c5fe9a
Maqueda Paul <maquedapablodavid@gmail.com>
2022-04-22 00:03:59 -0300

    archivos creados y seteados

---
+++ kernel/src/init.c
@@ -0,0 +1,6 @@
+#include "../include/init.h"
+
+void cerrar_programa(t_log *logger)
+{
+    log_destroy(logger);
+}
---
+++ shared/include/manager.h
@@ -0,0 +1,12 @@
+#ifndef MANAGER_H_
+#define MANAGER_H_
+#include <commons/log.h>
+#include <commons/config.h>
+#include "../include/sockets.h"
+t_config *iniciar_config(char *path);
+
+void terminar_programa(int conexion, t_log *logger, t_config *config);
+
+t_log *iniciar_logger(char *path, char *name, int debug, int log_level);
+
+#endif
