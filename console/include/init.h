2e4c4de930bc4fa1c89457fb2234a43a220cb29b
Maqueda Paul <maquedapablodavid@gmail.com>
2022-04-22 00:03:11 -0300

    añadido nuevos includes

--- console/include/init.h
+++ console/include/init.h
@@ -3,6 +3,8 @@
 
 #include "../../shared/include/sockets.h"
 #include "../../shared/include/protocolo.h"
+#include "../../shared/include/manager.h"
+#include <commons/config.h>
 
 bool generar_conexiones(t_log *logger, int *fd_mod2, int *fd_mod3);
 void cerrar_programa(t_log *logger);
--- kernel/include/main.h
+++ kernel/include/main.h
@@ -1,4 +1,15 @@
 #ifndef KERNEL_MAIN_H_
 #define KERNEL_MAIN_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+#include <commons/log.h>
+
+#include "init.h"
+#include "server.h"
+#include "../../shared/include/sockets.h"
+#include "../../shared/include/manager.h"
+
 #endif
