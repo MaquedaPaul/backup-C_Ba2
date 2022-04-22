648b3eff6877ee341f30c26234416fdd97214948
Maqueda Paul <maquedapablodavid@gmail.com>
2022-04-21 22:06:16 -0300

    config cargada a traves de archivo .config

--- kernel/main.c
+++ kernel/main.c
@@ -12,11 +12,14 @@ void sighandler(int s)
 int main()
 {
     signal(SIGINT, sighandler);
+    logger = iniciar_logger("kernel.log", "KERNEL", true, LOG_LEVEL_INFO);
 
-    logger = log_create("kernel.log", "KERNEL", true, LOG_LEVEL_INFO);
-
-    // No hardcodear IP y puerto, leer de config
-    fd_kernel = iniciar_servidor(logger, "KERNEL", "0.0.0.0", "6969");
+    t_config *configKernelAsServer = iniciar_config("./server.config");
+    ip = config_get_string_value(configKernelAsServer, "IP");
+    log_info(logger, "IP Cargada %s", ip);
+    puerto = config_get_string_value(configKernelAsServer, "PUERTO");
+    log_info(logger, "Puerto Cargado %s", puerto);
+    fd_kernel = iniciar_servidor(logger, "KERNEL", ip, puerto);
     while (server_escuchar(logger, "KERNEL", fd_kernel))
         ;
 
