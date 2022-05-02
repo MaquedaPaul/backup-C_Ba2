4bc5574e6c912f52581b5efe535eee356c1ff558
Maqueda Paul <maquedapablodavid@gmail.com>
2022-05-01 21:14:08 -0300

    Base para main: falta implementar {init, cargar config, cargar memoria}

--- memory/main.c
+++ memory/main.c
@@ -2,5 +2,22 @@
 
 int main()
 {
-    return 0;
+    if (!init() || !cargar_configuracion("memory.config") || !cargar_memoria()) // cada función va a hacer su operación, si las tres fallan, se cierra la app
+    {
+        cerrar_programa();
+        return EXIT_FAILURE;
+    }
+
+    // ****** CREACION DEL SERVIDOR ******
+    char *puerto = string_itoa(cfg->PUERTO);
+    memory_server = iniciar_servidor(logger, SERVERNAME, "0.0.0.0", puerto);
+    free(puerto);
+
+    //****** ESCUCHANDO ******
+    while (server_escuchar(SERVERNAME, memory_server))
+        ;
+
+    liberar_conexion(&memory_server);
+    cerrar_programa();
+    return EXIT_SUCCESS;
 }
