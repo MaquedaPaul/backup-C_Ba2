5974179a5183ad57980a5bdcc8eb3167d21793c8
Maqueda Paul <maquedapablodavid@gmail.com>
2022-05-26 18:52:11 -0300

    Agrego nuevos protocolos

--- shared/include/protocolo.h
+++ shared/include/protocolo.h
@@ -19,15 +19,22 @@
 
 ///
 
-typedef enum 
+typedef enum
 {
-        NO_OP,
+        NO_OP = 1,
         IO,
         READ,
         COPY,
         WRITE,
-        EXIT,
-}  op_code;
+        EXIT = 6,
+        // Memoria espera mensajes de CPU Y KERNEL:
+        START_PROCESS = 7,
+        SUSPEND_PROCESS,
+        END_PROCESS,
+        ACCESS_TO_TABLE_PAGES,
+        ACCESS_TO_USER_SPACE_READ,
+        ACCESS_TO_USER_SPACE_WRITE = 12
+} op_code;
 
 // bool send_aprobar_operativos(int fd, uint8_t nota1, uint8_t nota2);
 // bool recv_aprobar_operativos(int fd, uint8_t *nota1, uint8_t *nota2);
@@ -40,10 +47,9 @@ typedef enum
 bool send_unit32_t(int fd, uint32_t cantidad, op_code codigo_operacion);
 bool recv_uint32_t(int fd, uint32_t *cantidad);
 
-bool send_dos_uint32_t(int fd,uint32_t nota1, uint32_t nota2,op_code codigo_operacion);
-bool recv_dos_uint32_t(int fd,uint32_t *nota1,uint32_t *nota2);
+bool send_dos_uint32_t(int fd, uint32_t nota1, uint32_t nota2, op_code codigo_operacion);
+bool recv_dos_uint32_t(int fd, uint32_t *nota1, uint32_t *nota2);
 
 bool send_exit(int fd);
-
 
 #endif
