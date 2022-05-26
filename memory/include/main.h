02b762d12dfad1439d47142be34c8ed81789c4b3
Maqueda Paul <maquedapablodavid@gmail.com>
2022-05-25 23:52:25 -0300

    minor fix y agrego includes

--- memory/include/main.h
+++ memory/include/main.h
@@ -2,5 +2,13 @@
 #define MEMORY_MAIN_H_
 
 #define SERVERNAME "MEMORY_SERVER"
+#include <commons/log.h>
+#include <commons/config.h>
+#include <commons/memory.h>
+
+#include "init_memory.h"
+#include "../../shared/include/utils.h"
+#include "../../shared/include/sockets.h"
+#include "../../shared/include/protocolo.h"
 
 #endif
