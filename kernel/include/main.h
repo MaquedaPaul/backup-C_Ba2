9989a7a13ee5cb975d6b6d2b9fdcb814ab3ed88d
Maqueda Paul <maquedapablodavid@gmail.com>
2022-06-02 02:01:32 -0300

    añado los defines e includes necesarios

--- kernel/include/main.h
+++ kernel/include/main.h
@@ -4,12 +4,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
-
 #include <commons/log.h>
-
-#include "init.h"
+#include <commons/string.h>
+#include "../include/init_kernel.h"
 #include "server.h"
+#include <stdlib.h>
 #include "../../shared/include/sockets.h"
 #include "../../shared/include/manager.h"
+
+#define SERVERNAME "KERNEL_SERVER"
+t_log *logger;
+int kernel_server;
+int pid_actual = 0;
+char *puerto;
 
 #endif
