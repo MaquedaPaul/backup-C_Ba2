e320153e826cdba665902469a090d956de48b8d7
Maqueda Paul <maquedapablodavid@gmail.com>
2022-06-01 21:12:29 -0300

    añado estructura de instrucción

--- kernel/include/estructuras.h
+++ kernel/include/estructuras.h
@@ -1,5 +1,8 @@
 #ifndef KERNEL_ESTRUCTURAS_H
 #define KERNEL_ESTRUCTURAS_H
+#include <inttypes.h> //tipos de int
+#include <commons/list.h>
+#define MAX_CONSOLES = 100;
 
 typedef struct
 {
@@ -9,5 +12,14 @@ typedef struct
     uint16_t program_counter;
     float estimacion_rafaga;
 } t_PCB;
+typedef struct
+{
+    char *nombre;
+    uint32_t campo1;
+    uint32_t campo2;
+} t_instruccion;
+
+int socketsConsola[MAX_CONSOLES];
+void asignarPID(int pid, t_PCB *pcb);
 
 #endif
