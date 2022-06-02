4dab380f6f985e1d9b2b21e1b8214a964b0b113a
Maqueda Paul <maquedapablodavid@gmail.com>
2022-06-02 02:02:59 -0300

    añado handshake + expresividad en uint32_t

--- shared/src/protocolo.c
+++ shared/src/protocolo.c
@@ -1,19 +1,68 @@
 #include "../include/protocolo.h"
 
-static void *serializar_uint32_t(uint32_t cantidad, op_code codigoOperacion)
+static void *serializar_handshake(uint32_t handshake)
+{
+
+    void *stream = malloc(sizeof(op_code) + sizeof(uint32_t));
+    op_code codigoOperacion = HANDSHAKE;
+    // memcpy(donde lo guarda,lo que va a ser copiado,tamaño de lo que va a guardar)
+    memcpy(stream, &codigoOperacion, sizeof(op_code));
+    memcpy(stream + sizeof(op_code), &handshake, sizeof(uint32_t));
+    return stream;
+}
+
+static void deserializar_handshake(void *stream, uint32_t *handshake)
+{
+    memcpy(handshake, stream, sizeof(uint32_t));
+}
+
+// send(int socket, stream , tamaño Total del stream , 0)
+
+bool sendHandShake(int fd, uint32_t cantidad)
+{
+    op_code codigo_operacion = HANDSHAKE;
+    size_t size = sizeof(op_code) + sizeof(uint32_t);
+    void *stream = serializar_handshake(cantidad);
+    if (send(fd, stream, size, 0) != size)
+    {
+        free(stream);
+        return false;
+    }
+    free(stream);
+    return true;
+}
+
+bool recvHandShake(int fd, uint32_t *cantidad)
+{
+    size_t size = sizeof(uint32_t);
+    void *stream = malloc(size);
+
+    if (recv(fd, stream, size, 0) != size)
+    {
+        free(stream);
+        return false;
+    }
+
+    deserializar_handshake(stream, cantidad);
+
+    free(stream);
+    return true;
+}
+
+static void *serializar_uint32_t(uint32_t unEntero, op_code codigoOperacion)
 {
 
     void *stream = malloc(sizeof(op_code) + sizeof(uint32_t));
 
     // memcpy(donde lo guarda,lo que va a ser copiado,tamaño de lo que va a guardar)
     memcpy(stream, &codigoOperacion, sizeof(op_code));
-    memcpy(stream + sizeof(op_code), &cantidad, sizeof(uint32_t));
+    memcpy(stream + sizeof(op_code), &unEntero, sizeof(uint32_t));
     return stream;
 }
 
-static void deserializar_uint32_t(void *stream, uint32_t *cantidad)
+static void deserializar_uint32_t(void *stream, uint32_t *unEntero)
 {
-    memcpy(cantidad, stream, sizeof(uint32_t));
+    memcpy(unEntero, stream, sizeof(uint32_t));
 }
 
 // send(int socket, stream , tamaño Total del stream , 0)
